a5cddfaf23a7aea68f024dac55b85f957aa5f467
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-01 22:35:22 +0200

    Create submit.js

---
+++ commands/submit.js
@@ -0,0 +1,23 @@
+exports.run = (client, message, args) => {
+const Discord = require("discord.js")
+
+let type = args[0]
+let subchan = client.channels.get("660121646958379044")
+let att = message.attachments.first()
+
+if(type == "map") {
+if(message.attachments.first()){//checks if an attachment is sent
+        if(att.filename === `png`){//Download only png (customize this)    
+           subchan.send(`Map submitted by ${message.author.tag}`, { files: [`${att}`]});
+    }
+  }
+}
+
+
+if(type == "model"){
+if(message.attachments.first()){//checks if an attachment is sent
+        if(att.filename === `png`){//Download only png (customize this)
+      }
+    }
+  }
+}
