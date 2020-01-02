48d661a5e40531d1dbf6cfc26a5123126af20feb
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-02 13:03:43 +0200

    Update submit.js

--- commands/submit.js
+++ commands/submit.js
@@ -5,11 +5,14 @@ let type = args[0]
 let subchan = client.channels.get("660121646958379044")
 let att = message.attachments.first()
 
+subchan.send("Command Works")
+
 if(type == "map") {
 if(message.attachments.first()){//checks if an attachment is sent
-        if(att.filename === `png`){//Download only png (customize this)    
+//        if(att.filename === `png`){//Download only png (customize this)    
            subchan.send(`Map submitted by ${message.author.tag}`, { files: [`${att}`]});
-    }
+     subchan.send(att.filename
+ //   }
   }
 }
 
