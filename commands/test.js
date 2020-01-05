64df27a5992d39a5166a8eb962e352be5e54296f
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-05 12:40:18 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-
+const Discord = require("discord.js")
      (async () => {
     const captureWebsite = require('capture-website');
     await captureWebsite.file('https://spsknm.edupage.org/substitution/', 'suplovanie.png');
