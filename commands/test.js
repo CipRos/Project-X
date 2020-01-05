eba82f75d0245dec16ab9ca58ce9c35c58e868dc
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-05 12:21:05 +0200

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+
+     (async () => {
+    const captureWebsite = require('capture-website');
+    await captureWebsite.file('https://spsknm.edupage.org/substitution/', 'suplovanie.png');})();
+    const suplovanie = new Attachment("./suplovanie.png");
+    msg.channel.send(suplovanie);
+})();
+
+}
