32afc0d9e38e9de3cc23ad60dfa3c64192ca05e2
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-06 17:46:40 +0200

    Update and rename spam to spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -1,5 +1,8 @@
 exports.run = (client, message, args) => {
+exports.run = (client, message, args) => {
+const config = require("../config.json");
 
+if(message.author.id !== config.ownerID) return message.channel.send("You are not my developer!")
 let timestorun = args[0]
 let messagetosay = args.slice("2").join(" ")
 
