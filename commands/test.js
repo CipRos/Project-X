3bad6093bfabbe887984d1c835fd1c430be405a9
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-09 15:22:04 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,10 +3,10 @@ exports.run = (client, message, args) => {
 let iuser = message.mentions.users.first() || client.users.get(args[0])
 
 try {
-
+if(!iuser) return message.channel.send("User not found or was not specified!")
 message.guild.fetchAuditLogs({
 type: 'MEMBER_UPDATE', 
-user: `${iuser}`}
+user: `${iuser.id}`}
 ).then(async (audit) => { 
 let log = audit.entries.first().changes 
 message.channel.send(log) 
