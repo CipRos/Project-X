f1ddde86b9123d45b0c389b94f7fd87b2dc98375
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-19 19:53:38 +0200

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -7,21 +7,12 @@ try {
 let targChan = message.guild.channels.find(c => c.id == "667870313329066000") || null // find the targeted channel
 let messageid = targChan.fetchMessages("668445132735119374")
 
-      if (targChan) { // make sure that the targeted channel exists, if it exists then fetch its last message
-        targChan
-          .fetchMessages({ limit: 1 })
-          .then(f =>
-
-            
+      if (!targChan) return message.channel.send("No targChan");
+targChan.fetchMessages({ limit: 1 }).then(f =>
             //f.first().edit(`${f.first().content}`)
-            )
             f.first().edit("b")
-            message.channel.send(`:white_check_mark: | <#667870313329066000> has been updated`) // send the last message to the initial channel
-          )
-      } else { //if target doesn't exist, send an error
-        message.channel.send("Target does not exist!");
-      }
-
+            message.channel.send(`:white_check_mark: | <#667870313329066000> has been updated`)
+)
 } catch(err) {
  message.reply(`Error: ${err}`)
 }
