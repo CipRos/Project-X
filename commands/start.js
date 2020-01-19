fb9b382a107c6351753e8ca9cb19e20b489c1e0f
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-19 19:43:46 +0200

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -4,7 +4,7 @@ let iuser = message.mentions.users.first() || client.users.get(args[0])
 
 try {
 
-let targChan = message.guild.channels.find(c => c.id == "667870313329066000") || null; // find the targeted channel
+let targChan = message.guild.channels.find(c => c.id == "667870313329066000") || null // find the targeted channel
 let messageid = targChan.fetchMessages("668445132735119374")
 
       if (targChan) { // make sure that the targeted channel exists, if it exists then fetch its last message
@@ -17,7 +17,7 @@ let messageid = targChan.fetchMessages("668445132735119374")
 *******************
 */
             
-            if(!gotmessage) return message.reply("The Official Links Message does not exist")
+            if(!f.first()) return message.reply("The Official Links Message does not exist")
             //f.first().edit(`${f.first().content}`)
             f.first().edit("b")
             message.channel.send(`:white_check_mark: | <#667870313329066000> has been updated`) // send the last message to the initial channel
