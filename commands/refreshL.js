efc14de691302eb9402b560a00e3e660ed365f2a
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-19 20:25:48 +0200

    Update and rename refreshLinks.js to refreshL.js

--- commands/refreshL.js
+++ commands/refreshL.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
 try {
 
 let targChan = message.guild.channels.find(c => c.id == "667870313329066000") || null // find the targeted channel
-let messageid = targChan.fetchMessages("668518026588717056")
+let messageid = targChan.fetchMessages("668521067995988009")
 
 targChan.fetchMessages({ limit: 1 }).then(f => f.first().edit(`${f.first().content}`));
 message.channel.send(`:white_check_mark: | <#667870313329066000> has been updated`)
