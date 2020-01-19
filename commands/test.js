76b7347c75021541431dde2055e5311c6368e007
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-19 15:24:09 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,17 +3,26 @@ exports.run = (client, message, args) => {
 let iuser = message.mentions.users.first() || client.users.get(args[0])
 
 try {
-if(!iuser) return message.channel.send("User not found or was not specified!")
-message.guild.fetchAuditLogs({
-type: 'MEMBER_UPDATE', 
-user: `${iuser.id}`}
-).then(async (audit) => { 
-let log = audit.entries.first().changes
-let oldname = log.old
-let newname = log.new
-console.log(require('util').inspect(log))
-console.log(`${log} : ${newname} : ${oldname}`) 
-});
+
+const targChan = message.guild.channels.find(c => c.id == "667870313329066000") || null; // find the targeted channel
+const messageid = targchan.fetchMessages("668445132735119374")
+
+      if (targChan) { // make sure that the targeted channel exists, if it exists then fetch its last message
+        targChan
+          .fetchMessages({ limit: 1 })
+          .then(f =>
+/*
+*******************
+* Message Updater *
+*******************
+*/
+            message.channel.send(
+              `Last message from <#${target.id}> is...\n> ${f.first().content}`
+            ) // send the last message to the initial channel
+          );
+      } else { //if target doesn't exist, send an error
+        message.channel.send("Target does not exist!");
+      }
 
 } catch(err) {
  message.reply(`Error: ${err}`)
