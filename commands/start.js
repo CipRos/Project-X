44711674b49cbc4266cce22324800a0051033275
CipRos <42282352+CipRos@users.noreply.github.com>
2020-01-19 20:00:59 +0200

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -5,7 +5,7 @@ try {
 let targChan = message.guild.channels.find(c => c.id == "667870313329066000") || null // find the targeted channel
 let messageid = targChan.fetchMessages("668445132735119374")
 
-targChan.fetchMessages({ limit: 1 }).then(f => f.first().edit("b"));
+targChan.fetchMessages({ limit: 1 }).then(f => f.first().edit(`${f.first().content}`));
 message.channel.send(`:white_check_mark: | <#667870313329066000> has been updated`)
 } catch(err) {
 //f.first().edit(`${f.first().content}`)
